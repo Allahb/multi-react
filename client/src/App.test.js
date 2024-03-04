770e9e4872f96a1bf862e88e07a23ce5c2f1fffa
EC2 Default User <ec2-user@ip-172-31-81-83.ec2.internal>
2024-03-04 11:31:55 +0000

    Reinitialize repository

---
+++ client/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react';
+import App from './App';
+
+
+
+it('render without crashing', () => {});
+
+/*test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}); */
